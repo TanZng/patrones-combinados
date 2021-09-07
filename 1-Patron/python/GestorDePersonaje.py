2a9dfc339b76f28eeeb55842aa600ba65219ea2e
Tan Pipefy 🔥 <tania.zuniga@pipefy.com>
2021-09-07 11:02:44 -0500

    Patron 1 python inicio

---
+++ 1-Patron/python/GestorDePersonaje.py
@@ -0,0 +1,18 @@
+class GestorDePersonaje:
+    def __init__(self, personaje):
+        self._personaje = personaje
+
+    def mejorarUltimate(self):
+        pass
+
+    def mejorarUltimate(self):
+        pass
+
+    def mejorarArmadura(self):
+        pass
+
+    def mejorarArma(self):
+        pass
+
+    def verStats(self):
+        pass
