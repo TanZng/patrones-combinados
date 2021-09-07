e5211941b6fb8e4f53a17772d50e2e56f9aba1fa
Tan Pipefy 🔥 <tania.zuniga@pipefy.com>
2021-09-07 11:25:57 -0500

    Add fake SteamAPI and Personaje class

---
+++ 1-Patron/python/Personaje.py
@@ -0,0 +1,10 @@
+class Personaje(object):
+
+    def __init__(self, ultimate, arma, armadura, experiencia):
+        self.ultimate = ultimate
+        self.arma = arma
+        self.armadura = armadura
+        self.experiencia = experiencia
+
+    def verStats(self):
+        pass
---
+++ 1-Patron/python/SteamAPI.py
@@ -0,0 +1,6 @@
+class SteamAPI():
+    def __init__(self, account):
+        self.account = account
+
+    def authAccess(self):
+        pass
