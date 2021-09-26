cf101726ed147809ff834b4bf2eba172c58ca9ca
Felipe Valencia Tapia <nicoxlan4one@gmail.com>
2021-09-25 22:51:08 -0500

    Patrón 2 Armadura.php

--- 2-Patron/php/Armadura.php
+++ 2-Patron/php/Armadura.php
@@ -1,6 +1,62 @@
 <?php
 class Armadura
 {
+  private $nombre;
+  private $danoRecibido;
+  private $nivel;
+  private $duracion;
 
+  function setNombre()
+  {
+    return $this->nombre;
+  }
+
+  function getNombre($nNombre)
+  {
+    $this->nombre = $nNombre;
+  }
+
+  function setDanoRecibido()
+  {
+    return $this->danoRecibido;
+  }
+
+  function getDanoRecibido($nDano)
+  {
+    $this->danoRecibido = $nDano;
+  }
+
+  function setNivel()
+  {
+    return $this->nivel;
+  }
+
+  function getNivel($nNivel)
+  {
+    $this->nivel = $nNivel;
+  }
+
+  function setDuracion()
+  {
+    return $this->duracion;
+  }
+
+  function getDuracion($nDuracion)
+  {
+    $this->duracion = $nDuracion;
+  }
+
+  function incDañoRecibido($incremento)
+  {
+    $this->danoRecibido += $incremento;
+  }
+  function incDuracion($incremento)
+  {
+    $this->duracion += $incremento;
+  }
+  function incNivel()
+  {
+    $this->nivel += 1;
+  }
 }
 ?>
